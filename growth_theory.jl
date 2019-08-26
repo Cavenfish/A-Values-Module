81496efc89445410f6db6dea1d9c86b516cac725
Brian C. Ferrari <brianf1996@knights.ucf.edu>
2019-08-25 20:51:43 -0400

    did a basic plot for testing this, didnt work. close but wrong

--- growth_theory.jl
+++ growth_theory.jl
@@ -1,3 +1,4 @@
+using Plots
 
 #Hertz–Knudsen equation
 function sticking(α, ρ, M, T)
@@ -34,20 +35,24 @@ function pattern(t_rate, c, λ, θi, n)
     ϕ  = snell_θt(n[1], n[2], θi)
 
     #find all reflection going forward until hit window
-    for i = 1:3
+    for i = 1:2
         θt = snell_θt(n[i],n[i+1],θi)
-        append!( (sR(n[i],n[i+1],θi,θt) + pR(n[i],n[i+1],θi,θt)), R)
+        append!(R, (sR(n[i],n[i+1],θi,θt) + pR(n[i],n[i+1],θi,θt)))
         θi = θt
-        θt = snell_θt(n[i+1],n[i+2],θi)
+        if i+2 < 3
+            θt = snell_θt(n[i+1],n[i+2],θi)
+        end
     end
 
     #find all reflection going forward until back to vacuum
-    for j = 1:3
+    for j = 1:2
         i = 3 - (j-1)
-        θt = snell_θt(n[i],n[i+1],θi)
-        append!( (sR(n[i],n[i+1],θi,θt) + pR(n[i],n[i+1],θi,θt)), R)
+        θt = snell_θt(n[i],n[i-1],θi)
+        append!(R, (sR(n[i],n[i-1],θi,θt) + pR(n[i],n[i-1],θi,θt)))
         θi = θt
-        θt = snell_θt(n[i+1],n[i+2],θi)
+        if i-2 >= 1
+            θt = snell_θt(n[i-1],n[i-2],θi)
+        end
     end
 
     #Adjust R3 and R4 to account for transmission/reflection conservation
@@ -55,11 +60,28 @@ function pattern(t_rate, c, λ, θi, n)
     R[4] = (1-R[2])*R[4]  +  (1-R[3])*R[4]  +  (1-R[4])*R[4]
 
     #Convert growth rate to angle change rate
-    θ1 = (4*π)/(λ*cos(ϕ)) * t_rate
-    θ1 = (4*π)/(λ*cos(ϕ)) * t_rate * c
+    θ1 =  (4*π)/(λ*cos(ϕ)) .* t_rate
+    θ2 = ((4*π)/(λ*cos(ϕ)) .* t_rate) .* c
 
-    ( R[1] + R[2]*cos(θ1) ) - ( R[3] + R[4]*cos(θ2) )
+    ( R[1] .+ (R[2] .* cos.(θ1)) ) .+ ( R[3] .+ (R[4] .* cos.(θ2)) )
 end
 
+T    = 10
+M    = .01801528
+α    = 1
+ρ    = 6.6661e-5
+size = 2.5e-10
+c    = 0.25
+λ    = 405e-9
+θi   = 0.785398
+n    = [1, 1.309, 1.527]
+time = 1:0.1:100
+
 #Particles/Molecules per second times particle/molecule size = thickness rate
-t_rate = sticking(α, ρ, M, T) * size
+t_rate = (sticking(α, ρ, M, T) * size) .* time
+
+
+y = pattern(t_rate, c, λ, θi, n)
+
+
+plot(time,y, ylims=(-0.2, 0.3))
